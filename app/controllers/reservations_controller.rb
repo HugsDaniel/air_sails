70483b6e7f6841cbcc665fa5fa284b82967be9d4
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-17 17:32:54 +0200

    created routes

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,7 @@
+class ReservationsController < ApplicationController
+  def accept
+  end
+
+  def deny
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trips, only: [:new, :create, :show, :index] do
+    resources :bookings, only: [:create]
+  end
+  resources :reservations, only: [] do
+    member do
+      patch 'accept'
+      patch 'deny'
+    end
+  end
+  resource :profile, only: [:show]
 end
