31bfe4c3603a023f56a4c68fd543545c592ce3b9
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-18 15:29:39 +0200

    prepared for merge

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,16 +1,6 @@
 class TripsController < ApplicationController
   before_action :set_trip, only: [:show]
 
-  def index
-    @trips = Trip.all
-  end
-
-  def new
-  end
-
-  def create
-  end
-
   def show
     @booking = Booking.new
   end
@@ -20,8 +10,4 @@ class TripsController < ApplicationController
   def set_trip
     @trip = Trip.find(params[:id])
   end
-
-  # def trip_params
-  #   params.require(:trip).permit(:captain, :departure_port, :arrival_port, :departure_date, :duration, :distance, :boat_name, :description, :capacity, :price)
-  # end
 end
