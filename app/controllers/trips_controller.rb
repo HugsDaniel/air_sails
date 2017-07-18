98dd3ffc15781239b1c030565e871ec6ae633802
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-18 14:01:29 +0200

    added show page for trips

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,26 @@
+class TripsController < ApplicationController
+  before_action :set_trip, only: [:show]
+
+  def index
+    @trips = Trip.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  # def trip_params
+  #   params.require(:trip).permit(:captain, :departure_port, :arrival_port, :departure_date, :duration, :distance, :boat_name, :description, :capacity, :price)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+
+  get 'trips/new'
+
+  get 'trips/create'
+
+  get 'trips/show'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
