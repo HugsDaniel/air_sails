3606a70744ba3210eb7026300c0759ac52d7feae
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-19 16:40:44 +0200

    Added dynamic display of trips in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  
+
   def home
+    @trips = Trip.all
+    @cards = @trips.sample(3)
   end
 end
