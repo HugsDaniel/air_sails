d5d5c80d0350ef039cfb884ec8945f50c6d1f2b0
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-20 16:40:52 +0200

    fixed bookings display bug

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
 
   def show
     @trips = current_user.trips
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings.sort
   end
 end
